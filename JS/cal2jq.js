0fd3a328064d0581ce1595e241c50a1fdda2623a
mayurvaidya09 <mayur@newput.com>
2016-10-06 12:02:19 +0530

    JQuery file.

---
+++ JS/cal2jq.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  console.log("2");
+   $("a.date-clicker").bind("click", function() {
+  alert( "User clicked on 'foo.'" );
+});
+  //   var clickedDate = $(this).attr('data-date');
+  //   console.log(clickedDate);
+  //   //dayfinder();
+  // });
+});

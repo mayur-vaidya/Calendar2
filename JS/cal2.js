8a047f6a94ef762093f1bb5066dd7ec26bcdcc1f
mayurvaidya09 <mayur@newput.com>
2016-10-06 12:01:36 +0530

    JS and jQuery for calendar.

--- JS/cal2.js
+++ JS/cal2.js
@@ -1,6 +1,27 @@
+$(document).ready(function(){
+  monthName();
+  dropDown();
+  dropYear();
+  weekDays();
+  monthDates();
+  // $("a").on("click", function() {
+  //   clickedDate = $(this).attr('data-date');
+  //   dayFinder();
+  //
+  //   //console.log(clickedDate);
+  // });
+  //window.setTimeout(function() {
+    //document.getElementById("dayArea").innerHTML = "";
+
+  //},1000);
+});
+
+
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var year = 2000;
 var month = 0;
+var clickedDate;
+var totalDays = [];
 function monthName(){
   var today = new Date();
   today.setFullYear(year);
@@ -13,6 +34,8 @@ function monthName(){
 
   month = newMonth;
   document.getElementById("monthName").innerHTML = monthNames[newMonth] + " " + newYear;
+  // choosenYear(newYear);
+  // choosenMonth(newMonth);
 }
 
 function weekDays() {
@@ -49,9 +72,15 @@ function monthDates(){
   for( i = i; i <= end; i++, k++)
   {
 
-    m += "<td class='fullDates'>";
+    m += "<td class='fullDates'><a class='date-clicker' data-date='";
+    m += i
+    m +="'";
+    //m +="onclick='dayFinder("
+    //m += i;
+    //m +=")'";
+    m += ">";
     m += i;
-    m +="</td>";
+    m +="</a></td>";
 
     if(k%7 === 0){
       m += "</tr><tr>";
@@ -59,6 +88,12 @@ function monthDates(){
   }
 m += "</tr></table>";
 document.getElementById("dates").innerHTML = m;
+$("a").bind("click", function() {
+  clickedDate = $(this).attr('data-date');
+  dayFinder();
+
+  //console.log(clickedDate);
+});
 }
 
 function dropDown(){
@@ -104,11 +139,97 @@ function dropYear(){
     document.getElementById("currentDay").innerHTML = d;
   }
 
+  // function selector(i){
+  //   var date = i;
+  //   return date;
+  //
+  //   //console.log(date);
+  // }
+
+  // function choosenYear(years){
+  //   var choiceYear = years;
+  //   return choiceYear;
+  //   //console.log(choiceYear);
+  // }
+  //
+  // function choosenMonth(months){
+  //   var choiceMonth = months;
+  //   return choiceMonth;
+  //   //console.log(choiceMonth);
+  // }
+
+  function dayFinder() {
+    //var clickedDate = i;
+    var clickedMonth = month;
+    var clickedYear = year;
+    //console.log(clickedDate+" "+clickedMonth+" "+clickedYear);
+    var selectorDate = new Date();
+    selectorDate.setFullYear(clickedYear);
+    selectorDate.setMonth(clickedMonth);
+    selectorDate.setDate(clickedDate);
+    console.log(selectorDate);
+    var clickedDay = selectorDate.getDay();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    document.getElementById("dayArea").innerHTML = "Selected Date's days are: ";
+    totalDays.push(selectorDate);
+    if(totalDays.length == 2)
+    {
+      //var compareDate = new Date();
+      if(totalDays[0].getTime() > totalDays[1].getTime())
+      {
+        $("#dayArea").empty();
+        $("#dayArea").append("The input is not valid!!! Please enter dates in increasing order.");
+      }
+      else{
+      var current;
+      var oneDay = 24*60*60*1000;
+      var diffDays = Math.round(Math.abs((totalDays[0].getTime() - totalDays[1].getTime())/(oneDay)));
+      //console.log(diffDays);
+      var dateArray = [];
+      selectorDate.setTime(totalDays[0].getTime());
+      var day = selectorDate.getDay();
+      for(var a = 0;a <= diffDays; a++){
+        dateArray[a] = day;
+        if(dateArray[a] > 6) {
+          dateArray[a] -= 7;
+        }
+        day++;
+        //console.log(dateArray);
+      }
+      // if(totalDays[0].getDate() > totalDays[1].getDate())
+      // {
+      // }
+      if(dateArray.length < 7){
+       for(var v = 0; v < dateArray.length ; v++){
+        current = dateArray[v];
+          $("#dayArea").append(days[current]+" ");
+          console.log();
+       }}
+       else{
+         for(var v = 0; v < days.length ; v++){
+            $("#dayArea").append(days[v]+" ");
+         }
+        // console.log(days);
+       }
+     }
+    }
+    if(totalDays.length > 2){
+      totalDays = [];
+      dateArray = [];
+    }
+  }
+
 
 function loader(){
-  monthName();
-  dropDown();
-  dropYear();
-  weekDays();
-  monthDates();
+
 }
+
+  //console.log("2");
+  //  $("a").click(function(){
+  //    console.log("1");
+  //  });
+
+  //   var clickedDate = $(this).attr('data-date');
+  //   console.log(clickedDate);
+  //   //dayfinder();
+  // });
